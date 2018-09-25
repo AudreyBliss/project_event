8e9163e1443cdfd04354543f715b564dd2e6795f
AudreyBliss <audrey94300@hotmail.fr>
2018-09-25 11:20:20 +0200

    moodification

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,12 +16,56 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+            'var_test' => 'Hello world'
+    ]);
+
     }
 
-    
-    public function exempleAction(Request $request)
+
+
+     /**
+     * @Route("/events", name="events")
+     */
+
+
+    public function eventAction(Request $request)// fonction pour afficher tout les evenements
     {
-        # code...
+       $curl = $this -> get('AppBundle\Network\ServiceCurl');
+
+       $events = $this -> getEvents();
+       $gpsEvents = [];
+       
+       foreach($events as $e) {
+           $adresse = str_replace(' ', '+', $e['adresse']);
+           $suggestions = json_decode($curl->curl_get($adresse),true);
+           $gps = $suggestions['features'][0]['geometry']['coordinates'];
+           $e['latitude'] = $gps[1];
+           $e['longitude'] = $gps[0];
+           $gpsEvents[] = $e;
+       } 
+       
+       return $this->render('@App/event/events.html.twig', [
+        'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        'events' => $gpsEvents
+    ]);
+    
     }
+
+
+    protected function getEvents(){
+    $events = [
+        
+            ['nom'=>'Nouvelle année', 'date'=>'01/01/2019', 'adresse'=>'Place du Trocadéro 75016 Paris'],
+            ['nom'=>'Mon anniversaire', 'date'=>'15/05/2019','adresse'=>'Place de la Bastille 75011 Paris'],
+            ['nom'=>'Cinéma en plein air', 'date'=>'20/09/2018', 'adresse'=>'70 rue de la Liberté 75008 Paris'],
+            ['nom'=>'Feux d\'artifices', 'date'=>'05/11/2018', 'adresse'=>'70 avenue de la Révolution 75008 Paris']
+
+            ];
+    
+            return $events;
+    }
+
+
+
 }
+
