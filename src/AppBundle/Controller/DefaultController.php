1c1260aa3bb0bb0687efed3fe3f77d3c970bd08d
AudreyBliss <audrey94300@hotmail.fr>
2018-10-03 17:19:03 +0200

    $adresse2

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -36,9 +36,8 @@ class DefaultController extends Controller
        $gpsEvents = [];      
        foreach($events as $e) {
            /*$adresse = str_replace(' ', '+', $e['adresse']);*/
-          /* $adresse = str_replace(' ', '+', $e->adresse);*/
+          $adresse = str_replace(' ', '+', ($e->adresse));
            /*$adresse = str_replace(' ', '+', $e('adresse'));*/
-           $adresse;
            $suggestions = json_decode($curl->curl_get($adresse),true);
            $gps = $suggestions['features'][0]['geometry']['coordinates'];
            $e['latitude'] = $gps[1];
