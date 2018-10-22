06e9392dad4934114a59de5f5f65292d3ea315a8
AudreyBliss <audrey94300@hotmail.fr>
2018-10-22 11:57:07 +0200

    function display_marker

--- web/public/js/leaflettt.js
+++ web/public/js/leaflettt.js
@@ -10,12 +10,13 @@ function display_map (mymap)
 
 }
 
-function display_marker(mymap, latitude, longitude){
+function display_marker(mymap, latitude, longitude, name){
 
     /*L.marker([latitude, longitude]).addTo(mymap);*/
    
     L.marker([latitude, longitude]).addTo(mymap)
-    .bindPopup()
+
+    .bindPopup(name)
     .openPopup();
 }
 
@@ -27,7 +28,9 @@ function get_markers(){
   .then(function(myJson) {
     console.log(JSON.stringify(myJson));
     //display_marker(mymap, latitude, longitude)
-        for (let events of myJson ){
+        for (let events of myJson )
+        {
+            console.log(events)
             display_marker(mymap, events.latitude,events.longitude, events.nom)
            
         }  
