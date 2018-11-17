b098d919390f67a62f9f3a4b5c9a9722296a2345
AudreyBliss <audrey94300@hotmail.fr>
2018-11-17 11:04:48 +0100

    chgmt id

--- src/AppBundle/Controller/ProjectController.php
+++ src/AppBundle/Controller/ProjectController.php
@@ -130,7 +130,7 @@ class ProjectController extends Controller
          $em = $this->getDoctrine()->getManager();
          switch ($request->query->get('option')) {
             case 'pastEvent':
-                $events = $em -> getRepository('AppBundle:Project')->pastEvent(); 
+                $events = $em -> getRepository('AppBundle:Project')->findpastEvent(); 
                 break;
             case 'futurEvent':
                 $events = $em -> getRepository('AppBundle:Project')->findfuturEvent(); 
